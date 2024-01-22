d1b534d7fd292eab3623b2d50b9d669a3840cc7d
ethanLinHoucheng <lin_houcheng_ethan@s2021.ssts.edu.sg>
2024-01-22 11:40:52 +0800

    vector stores and query functions

--- llm_model.py
+++ llm_model.py
@@ -4,51 +4,51 @@ warnings.filterwarnings("ignore")
 import os
 from dotenv import load_dotenv, find_dotenv
 
-from langchain.document_loaders import YoutubeLoader
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import PyPDFium2Loader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.llms import OpenAI
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
-import chromadb
+from langchain.document_loaders import DirectoryLoader
 
 load_dotenv()
 
 #get API Key
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
+llm = OpenAI(openai_api_key=openai_api_key,model_name='gpt-3.5-turbo-instruct')
+
 #upload pdf files and embed them.
 
 def embed():
-    print("start")
-    pdf_folder_path = "./papers/test_folders"
-    documents = []
-    for file in os.listdir(pdf_folder_path):
-        if file.endswith('.pdf'):
-            pdf_path = os.path.join(pdf_folder_path, file)
-            loader = PyPDFium2Loader(pdf_path)
-            documents.extend(loader.load())
-    text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=10)
-    chunked_documents = text_splitter.split_documents(documents)
-    client = chromadb.Client()
-    if client.list_collections():
-        consent_collection = client.create_collection("consent_collection")
-    else:
-        print("Collection already exists")
-    vectordb = Chroma.from_documents(
-        documents=chunked_documents,
-        embedding=OpenAIEmbeddings(openai_api_key=openai_api_key,persist_directory="./vector_db")
+    loader = DirectoryLoader('./test_folders', glob="./*.pdf", loader_cls=PyPDFium2Loader)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    texts = text_splitter.split_documents(documents)
+    persist_directory="./vector_db"
+    embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+
+    vectordb = Chroma.from_documents(documents=texts,   
+        embedding=embedding,
+        persist_directory=persist_directory
     )
-    print("end")
+    vectordb.persist()
+    vectordb = None
+    vectordb = Chroma(persist_directory=persist_directory, 
+    embedding_function=embedding
+    )
     return vectordb
 
-
 vectordb = embed()
+retriever = vectordb.as_retriever()
+docs = retriever.get_relevant_documents("How much money did Pando raise?")
+retriever = vectordb.as_retriever(search_kwargs={"k": 2})
+qa_chain = RetrievalQA.from_chain_type(llm=llm, 
+                                  chain_type="stuff", 
+                                  retriever=retriever, 
+                                  return_source_documents=True)
 
-
-
-
-#llm = OpenAI(openai_api_key=openai_api_key)
-
+query = "what is a phonon mediated decay of an atom"
+llm_response = qa_chain(query)
+print(llm_response['result'])
