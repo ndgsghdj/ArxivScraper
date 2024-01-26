c576267479793f79adc20b1517d80845d6339719
Fidele <118550160+FP31A@users.noreply.github.com>
2024-01-26 11:17:46 +0800

    Update main.py

--- frontend/main.py
+++ frontend/main.py
@@ -3,7 +3,21 @@ import time
 
 
 def main(page: ft.Page):
-    pass
+    page.title = "PDF reader"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    def btn_click(e):
+        if not txt_name.value:
+            txt_name.error_text = "Please Enter PDF file needed"
+            page.update()
+        else:
+            PDF = txt_name.value
+            page.clean()
+
+    txt_name = ft.TextField(label="PDF Link")
+
+    page.add(txt_name, ft.ElevatedButton("Enter", on_click=btn_click))
 
 
-ft.app(main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
