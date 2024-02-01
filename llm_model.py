ac2559decd5365c2f7743507125d3f8fd6c4f482
ethanLinHoucheng <lin_houcheng_ethan@s2021.ssts.edu.sg>
2024-02-01 18:24:57 +0800

    idk

--- llm_model.py
+++ llm_model.py
@@ -23,39 +23,7 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 #call model
 llm = OpenAI(openai_api_key=openai_api_key,model_name='gpt-3.5-turbo-instruct',temperature=0,top_p=1,frequency_penalty=0,presence_penalty=0,best_of=1)
 
-
-
-#upload pdf files and embed them.
-
-# def embed():
-#     loader = DirectoryLoader('./papers/pdf/0704', glob="./*.pdf", loader_cls=PyPDFium2Loader)
-#     documents = loader.load()
-#     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-#     texts = text_splitter.split_documents(documents)
-#     persist_directory="./vector_db"
-#     embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
-
-#     vectordb = Chroma.from_documents(documents=texts,   
-#         embedding=embedding,
-#         persist_directory=persist_directory
-#     )
-#     vectordb.persist()
-#     vectordb = None
-#     vectordb = Chroma(persist_directory=persist_directory, 
-#     embedding_function=embedding
-#     )
-#     return vectordb
-
-#vectordb = embed()
-#retriever = vectordb.as_retriever()
-#docs = retriever.get_relevant_documents("Key definitions and ideas of rarely seen concepts")
-#retriever = vectordb.as_retriever(search_kwargs={"k": 2})
-#qa_chain = RetrievalQA.from_chain_type(llm=llm, 
-#    chain_type="stuff", 
-#    retriever=retriever, 
-#    return_source_documents=True
-#)
-
+#turn the llm response into a list to be used on UI side
 def listify_llm_response(response):
     ans = []
     curr = ""
@@ -68,10 +36,7 @@ def listify_llm_response(response):
     ans.append(curr)
     return ans
 
-
-        
-
-
+#query llm method based on query_text
 def query_llm(query_text):
     messages = [
         {"role": "system", "content": """In the following text, provide quotes ONLY FOUND IN THE TEXT containing physics phrases and keywords that are not commonly encountered in everyday conversation and text. For each quoted phrase, explain its meaning. If the explanation contains numerical numbers, use the words. For example, 1 should be one. 
@@ -91,4 +56,5 @@ def query_llm(query_text):
     )
     listified_response = listify_llm_response(response.choices[0].message.content.strip())
     return listified_response
-print(len(query_llm("General relativity states that gravity bends the fabric of space time. Light also moves at a constant speed")))
+
+
