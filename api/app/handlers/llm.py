cfd15a5d188ab8b43208cc60dad3f54862d7d476
ethanLinHoucheng <lin_houcheng_ethan@s2021.ssts.edu.sg>
2024-02-15 21:16:27 +0800

    idk

--- api/app/handlers/llm.py
+++ api/app/handlers/llm.py
@@ -4,12 +4,26 @@ warnings.filterwarnings("ignore")
 import os
 from dotenv import load_dotenv, find_dotenv
 
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import PyPDFium2Loader
+from langchain.document_loaders import DirectoryLoader
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
 load_dotenv()
 
-# get API Key
-openai_api_key = os.getenv("../OPENAI_API_KEY")
+#get API Key
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
-# turn the llm response into a list to be used on UI side
+#call model
+llm = OpenAI(openai_api_key=openai_api_key,model_name='gpt-3.5-turbo-instruct',temperature=0,top_p=1,frequency_penalty=0,presence_penalty=0,best_of=1)
+
+#turn the llm response into a list to be used on UI side
 def listify_llm_response(response):
     ans = []
     curr = ""
@@ -22,10 +36,33 @@ def listify_llm_response(response):
     ans.append(curr)
     return ans
 
-# query llm method based on query_text
+
+def truncate_text(text,chunk_size):
+    ans = []
+    counter = 0
+    curr=""
+    i = 0
+    while i < len(text):
+        
+        curr += text[i]
+        counter += 1
+        
+        if counter == chunk_size:
+            while i < len(text) and (text[i].isalpha() or text[i].isnumeric() or text[i] == "-" or text[i] == "_"):
+                curr += text[i]
+                i += 1
+            ans.append(curr)
+            curr = ""
+            counter = 0
+        i += 1
+    if curr != "":
+        ans.append(curr)
+    return ans
+
+#query llm method based on query_text
 def query_llm(query_text):
     messages = [
-        {"role": "system", "content": """In the following text, provide quotes ONLY FOUND IN THE TEXT containing physics phrases and keywords that are not commonly encountered in everyday conversation and text. For each quoted phrase, explain its meaning in laymen terms. If the explanation contains numerical numbers, use the words. For example, 1 should be one. 
+        {"role": "system", "content": """In the following text, provide quotes ONLY FOUND IN THE TEXT containing physics phrases and keywords that are not commonly encountered in everyday conversation and text. For each quoted phrase, explain its meaning. If the explanation contains numerical numbers, use the words. For example, 1 should be one. 
     another example: if query_text = "The car going around in a circle experiences centripetal force, which is equals to the frictional force acting on the car"
 
     the output would be: 
